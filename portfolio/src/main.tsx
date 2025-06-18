e84392535ab595f358e60070c7546f0dd3751722
Dylan ARAUJO LOPES <dylan.lopes51@gmail.com>
2025-06-18 18:38:32 +0200

    structure_projet
    Réorganisation des fichiers.
    Installation du routing
    Création des fichiers nécessaires pour les pages

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router";
 import './index.css'
-import App from './App.tsx'
+import HomePage from './HomePage/HomePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path={"/"} element={<HomePage/>}/>
+      </Routes>
+  </BrowserRouter>
 )
