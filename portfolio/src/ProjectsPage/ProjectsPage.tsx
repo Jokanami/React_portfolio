d09ce1acf3f7498bcf1de3c699f92bcb13192184
Dylan ARAUJO LOPES <dylan.lopes51@gmail.com>
2025-06-21 17:16:07 +0200

    preparation_frise_projets
    Création de la base de la frise chronologique des projets

--- portfolio/src/ProjectsPage/ProjectsPage.tsx
+++ portfolio/src/ProjectsPage/ProjectsPage.tsx
@@ -1,10 +1,100 @@
 import './ProjectsPage.css'
 
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 function ProjectsPage() {
 
 
     return (
-        <h1> Projets ! </h1>
+        <div className={"projects-page-container"}>
+
+            <h1 className={"title text-center mb-5 display-4 fw-bold"}>
+                Mes projets
+            </h1>
+
+
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#dddddd', color: '#111111' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #dddddd' }}
+                    date="2011 - present"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
+                >
+                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                    <p>
+                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#dddddd', color: '#111111' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #dddddd' }}
+                    date="2011 - present"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
+                >
+                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#dddddd', color: '#111111' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #dddddd' }}
+                    date="2011 - present"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
+                >
+                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                    <p>
+                        User Experience, Visual Design
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#dddddd', color: '#111111' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #dddddd' }}
+                    date="2011 - present"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
+                >
+                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                        User Experience, Visual Design
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#dddddd', color: '#111111' }}
+                    contentArrowStyle={{ borderRight: '7px solid  #dddddd' }}
+                    date="2011 - present"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
+                >
+                    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+                    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                    <p>
+                        Strategy, Social Media
+                    </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement
+                    iconStyle={{ background: '#00ff00' }}
+
+                />
+            </VerticalTimeline>
+
+
+        </div>
+
     )
 
 }
