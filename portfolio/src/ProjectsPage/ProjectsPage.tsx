c694ab68edf56efdf92812d023fff45f8a835d42
Dylan ARAUJO LOPES <dylan.lopes51@gmail.com>
2025-06-23 19:26:06 +0200

    update_titres_projets
    Changement des titres des projets pour la mise en avant de l'expérience.

--- portfolio/src/ProjectsPage/ProjectsPage.tsx
+++ portfolio/src/ProjectsPage/ProjectsPage.tsx
@@ -23,7 +23,7 @@ function ProjectsPage() {
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
-                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                    <h3 className="vertical-timeline-element-title">Projet Stavosph'air</h3>
                     <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                     <p>
                         Creative Direction, User Experience, Visual Design, Project Management, Team Leading
@@ -37,7 +37,7 @@ function ProjectsPage() {
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
-                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h3 className="vertical-timeline-element-title">Baccalauréat Général : Spécialité Mathématiques et NSI</h3>
                     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
                     <p>
                         Creative Direction, User Experience, Visual Design, SEO, Online Marketing
@@ -51,7 +51,7 @@ function ProjectsPage() {
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
-                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                    <h3 className="vertical-timeline-element-title">Développement d'un jeu Démineur</h3>
                     <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
                     <p>
                         User Experience, Visual Design
@@ -65,7 +65,7 @@ function ProjectsPage() {
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
-                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                    <h3 className="vertical-timeline-element-title">Développement d'une application bureau pour le centre ARHM de Dardilly</h3>
                     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
                     <p>
                         User Experience, Visual Design
@@ -79,7 +79,7 @@ function ProjectsPage() {
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
-                    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+                    <h3 className="vertical-timeline-element-title">BTS Systèmes Numériques : Informatique et Réseaux</h3>
                     <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
                     <p>
                         Strategy, Social Media
