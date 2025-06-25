c2a21801786d1e92f38f2fdc40aa2305ba26f773
Dylan ARAUJO LOPES <dylan.lopes51@gmail.com>
2025-06-25 17:10:41 +0200

    modif_link_header

--- portfolio/src/PageLayout/PageLayout.tsx
+++ portfolio/src/PageLayout/PageLayout.tsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router"
 import {Outlet} from "react-router";
 
 const PageLayout = () => {
@@ -7,13 +8,13 @@ const PageLayout = () => {
         <>
             <Navbar bg={"black"} variant={"dark"} expand="lg">
                 <Container>
-                    <Navbar.Brand href={"/"} >Mon Portfolio</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">Mon Portfolio</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className={"ms-auto"}>
-                            <Nav.Link href={"/contacts"}>Me contacter</Nav.Link>
-                            <Nav.Link href={"/projects"}>Mes projets et formations</Nav.Link>
-                            <Nav.Link href={"/skills"}>Mes compétences</Nav.Link>
+                            <Nav.Link as={Link} to="/contacts">Me contacter</Nav.Link>
+                            <Nav.Link as={Link} to="/projects">Mes projets et formations</Nav.Link>
+                            <Nav.Link as={Link} to="/skills">Mes compétences</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
